5990ab0eff329663c60a8e1e75c4ec69716ed241
Luis Ramírez <luisrmdev@gmail.com>
2026-08-02 16:48:55 -0600

    fix compatiblidad api google

--- test_api.py
+++
@@ -1,4 +0,0 @@
-import requests
-res = requests.get("http://localhost:8000/api/models")
-print(res.status_code)
-print(res.text)
--- test_models.py
+++
@@ -1,7 +0,0 @@
-from dotenv import load_dotenv
-load_dotenv()
-from google import genai
-client = genai.Client()
-models = list(client.models.list())
-print(dir(models[0]))
-print(models[0].supported_actions)
